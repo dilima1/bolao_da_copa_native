925aa593fa602368546650c12048e56b1fcc227a
Edilson Lima <edilson.lima1@hotmail.com>
2022-11-05 11:42:04 -0300

    finalizando a parte de autenticacao do Google

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { THEME } from './src/styles/theme';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { SignIn } from './src/screens/Signin';
 import { Loading } from './src/components/Loading';
+import { AuthContextProvider } from './src/contexts/AuthContext';
 
 
 export default function App() {
@@ -10,12 +11,14 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={ THEME }>
-      <StatusBar 
-        barStyle='light-content'
-        backgroundColor="transparent"
-        translucent
-      />
-      { fontsLoaded ? <SignIn /> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar 
+          barStyle='light-content'
+          backgroundColor="transparent"
+          translucent
+        />
+        { fontsLoaded ? <SignIn /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
